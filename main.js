4fb5e7256c83f0555d4a3a1158a58ba9547cb461
Chandan <cc2099338@gmail.com>
2021-08-04 18:32:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let daysItems = document.getElementById("days");
+let hoursItems = document.getElementById("hours");
+let minItems = document.getElementById("min");
+let secItems = document.getElementById("sec");
+
+let countDown = () => {
+    let targetDate = new Date("6 Aug 2021")
+    let currentDate = new Date();
+    let remainDay = targetDate - currentDate;
+    console.log(remainDay);
+
+    let days = Math.floor(remainDay/1000/60/60/24); //console.log(days)
+    let hours = Math.floor(remainDay/1000/60/60)%24; //console.log(hours)
+    let min = Math.floor(remainDay/1000/60)%60;
+    let sec = Math.floor(remainDay/1000)%60; //console.log(sec)
+
+    daysItems.innerHTML= days;
+    hoursItems.innerHTML = hours;
+    minItems.innerHTML = min;
+    secItems.innerHTML = sec;
+}
+
+countDown()
+setInterval(countDown, 1000)
